4d309b3b7b02bd3b54fc63d678f6770e3f568ecc
lihle-sibiya <129907260+lihle-sibiya@users.noreply.github.com>
2023-08-23 02:19:48 +0200

    second commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap-icons/font/bootstrap-icons.css';
 import React from 'react';
-//import firebase from 'firebase/compat/app';
-import 'firebase/compat/auth';
-import 'firebase/compat/firestore';
-import 'firebase/compat/database';
-import WeatherApp from './WeatherApp';
+import TodoApp from './TodoApp';
 
 class App extends React.Component {
   render(){
   return (
-    <WeatherApp />
+    <TodoApp />
   );
   }
 }
